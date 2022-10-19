1550d2587ee73b077adc73d9e5a3d7399732f2e6
Natthawat-CPE <aha0037@gmail.com>
2022-10-19 20:15:43 +0700

    add datetime-picker finish

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -15,7 +15,27 @@ import Grid from "@mui/material/Grid";
 
 import TextField from "@mui/material/TextField";
 
+import Autocomplete from "@mui/material/Autocomplete";
+
+import { FormControl } from "@mui/material";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import setSeconds from "date-fns/setSeconds";
+
 function App() {
+  const top100Films = () => [
+    { label: "The Shawshank Redemption", year: 1994 },
+    { label: "The Godfather", year: 1972 },
+    { label: "The Godfather: Part II", year: 1974 },
+    { label: "The Dark Knight", year: 2008 },
+    { label: "12 Angry Men", year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: "Pulp Fiction", year: 1994 },
+  ];
+
+  const [value, setValue] = React.useState<Date | null>(null);
+
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -62,35 +82,62 @@ function App() {
             </Box>
             <hr />
 
-            <Grid container spacing={2} sx={{ padding: 2 }}>
+            <Grid container spacing={5} sx={{ padding: 5 }}>
               <Grid item xs={6}>
                 <p>ชื่อผู้ป่วย</p>
-                <TextField
+                <Autocomplete
                   fullWidth
-                  id="outlined-basic"
-                  // label="Outlined"
-                  variant="outlined"
+                  disablePortal
+                  id="combo-box-demo"
+                  options={top100Films()}
+                  // sx={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="เลือก" />
+                  )}
                 />
               </Grid>
               <Grid item xs={6}>
                 <p>โภชนาการแบบ</p>
-                <TextField
+                <Autocomplete
                   fullWidth
-                  id="outlined-basic"
-                  // label="Outlined"
-                  variant="outlined"
+                  disablePortal
+                  id="combo-box-demo"
+                  options={top100Films()}
+                  // sx={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="เลือก" />
+                  )}
                 />
               </Grid>
               <Grid item xs={6}>
                 <p>แพทย์ผู้จัดการ</p>
-                <TextField
+                <Autocomplete
                   fullWidth
-                  id="outlined-basic"
-                  // label="Outlined"
-                  variant="outlined"
+                  disablePortal
+                  id="combo-box-demo"
+                  options={top100Films()}
+                  // sx={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Lock-Doctor" />
+                  )}
                 />
               </Grid>
-              <Grid item xs={8}>
+              <Grid item xs={6}>
+                <p>Date Time</p>
+                <FormControl fullWidth variant="outlined">
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                      label="Basic example"
+                      value={value}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
                 <p>หมายเหตุ</p>
                 <TextField
                   fullWidth
